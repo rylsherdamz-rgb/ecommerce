018d660d8c952a05c0fdc8979c819e5bb09273a3
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-01-28 20:49:29 +0800

    got the styling to be decent but not good but the functionality is correct

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,20 +1,14 @@
 // import the types for type safety
 
 export default function Carousel() {
-  return <div className="w-full border h-[20vh] max-sm:overflow-y-auto md:overflow-x-auto
+  return <div className="w-full border max-sm:py-5 md:h-[20vh]
+    max-sm:overflow-y-auto md:overflow-x-auto
+    max-sm:h-[50vh]
     max-sm:grid max-sm:grid-cols-2 md:flex max-sm:flex-col 
-    max-sm:gap-y-5 md:gap-x-10
+    max-sm:gap-y-2 md:gap-x-10
     md:flex-row  max-sm:
     max-sm:px-5 md:px-10 rounded-lg bg-green-200">
-    <div className="w-25 h-25 bg-blue-300">
-    </div>
-    <div className="w-25 h-25 bg-blue-300">
-    </div>
-<div className="w-25 h-25 bg-blue-300">
-    </div>
-
-
-
+   {/* add the card here  */}
 
   </div>
 }
