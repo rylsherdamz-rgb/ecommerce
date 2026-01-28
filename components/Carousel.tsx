0fe322cc68f970b5e26d2d8c2a1b40627f044e48
rylsherdamz-rgb <rylsherdamz@gmail.com>
2026-01-28 20:43:55 +0800

    adding style and responsiveness to the carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,7 +1,20 @@
 // import the types for type safety
 
 export default function Carousel() {
-  return <div className="w-full max-sm:px-5 md:px-10 rounded-lg bg-green-200">
+  return <div className="w-full border h-[20vh] max-sm:overflow-y-auto md:overflow-x-auto
+    max-sm:grid max-sm:grid-col-2 md:flex max-sm:flex-col 
+    max-sm:gap-y-5 md:gap-x-10
+    md:flex-row  max-sm:
+    max-sm:px-5 md:px-10 rounded-lg bg-green-200">
+    <div className="w-25 h-25 bg-blue-300">
+    </div>
+    <div className="w-25 h-25 bg-blue-300">
+    </div>
+<div className="w-25 h-25 bg-blue-300">
+    </div>
+
+
+
 
   </div>
 }
